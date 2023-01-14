997b0b2617ba85e5b978c0879d8edbcc607c8071
PPUDANI <86819878+PPUDANI@users.noreply.github.com>
2023-01-14 23:12:54 +0900

    [Gold V] Title: 별 찍기 - 10, Time: 228 ms, Memory: 6772 KB -BaekjoonHub

---
+++ 백준/Gold/2447. 별 찍기 － 10/별 찍기 － 10.cc
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void square(vector<vector<char>> &res, int num, int y, int x, char c)
+{
+    if(num == 1)
+    {
+        res[y][x] = c;
+    }
+    else
+    {
+        int count = 0;
+        for ( int i = y; i < y + num; i += num/3 )
+        {
+            for ( int j = x; j < x + num; j += num/3 )
+            {
+                if ( count == 4 )
+                {
+                    square(res, num/3, i, j, ' ');
+                }
+                else
+                {
+                    square(res, num/3, i, j, c);
+                }
+                count++;
+            }
+        }
+    }
+}
+
+int main()
+{
+    int num;
+    cin >> num;
+
+    vector<vector<char>> res(num, vector<char>(num));
+
+    square(res, num, 0, 0, '*');
+
+    //res 출력
+    for(int j = 0; j < num; j++)
+    {
+        for(char i : res[j])
+        {
+            cout << i;
+        }
+        cout << endl;
+    }
+}
