ac358657df402fd0e535f4eed977ec7b4c208562
PPUDANI <86819878+PPUDANI@users.noreply.github.com>
2026-01-07 19:36:15 +0900

    [Silver III] Title: 1로 만들기, Time: 4 ms, Memory: 5928 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.cc
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.cc
@@ -1,50 +1,35 @@
-#include <iostream>
-#include <queue>
+#include<iostream>
+#include<vector>
 using namespace std;
 
-struct Node
+int main() 
 {
-    int Num;
-    int Cnt;
-};
+	int n; cin >> n;
 
-int main()
-{
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-    int N;
-    cin >> N;
-    
-    int Cnt = 0;
-    queue<Node> Q;
-    Q.push({N, 0});
-    
-    while (true)
-    {
-        Node CurNode = Q.front();
-        Q.pop();
-        
-        if (CurNode.Num == 1)
-        {
-            Cnt = CurNode.Cnt;
-            break;
-        }
-        
-        CurNode.Cnt++;
-        
-        if (CurNode.Num % 3 == 0)
-        {
-            Q.push({CurNode.Num / 3, CurNode.Cnt});
-        }
-        
-        if (CurNode.Num % 2 == 0)
-        {
-            Q.push({CurNode.Num / 2, CurNode.Cnt});
-        }
-        
-        Q.push({CurNode.Num - 1, CurNode.Cnt});
-    }
-    
-    cout << Cnt;
+	vector<int> dp(1000001);
+	dp[1] = 0; 
+	dp[2] = 1; 
+	dp[3] = 1;
+
+
+	for (int i = 4; i <= 1000000; i++)
+	{
+		int count=0;
+
+		dp[i] = dp[i - 1] + 1;
+
+		if (i % 3 == 0) 
+		{ 
+			count = 1+dp[i / 3]; 
+			if (count < dp[i]){dp[i]=count;}
+		}
+		if (i % 2 == 0) 
+		{ 
+			count = 1 + dp[i / 2];
+			if (count < dp[i]) { dp[i] = count; }
+		}
+	}
+
+	cout << dp[n];
+	
 }
