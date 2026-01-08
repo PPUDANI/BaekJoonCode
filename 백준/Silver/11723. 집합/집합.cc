a204466575f438c951c2e7a147c28c8721736a98
PPUDANI <86819878+PPUDANI@users.noreply.github.com>
2026-01-08 20:12:17 +0900

    [Silver V] Title: 집합, Time: 720 ms, Memory: 2032 KB -BaekjoonHub

---
+++ 백준/Silver/11723. 집합/집합.cc
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <unordered_map>
+#include <unordered_set>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cout.tie(0);
+    cin.tie(0);
+    
+    unordered_map<string, int> Functions;
+    unordered_set<int> Numbers;
+    unordered_set<int> AlllNumber = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20};
+    unordered_set<int> Empty;
+    Functions.insert({"add", 0});
+    Functions.insert({"remove", 1});
+    Functions.insert({"check", 2});
+    Functions.insert({"toggle", 3});
+    Functions.insert({"all", 4});
+    Functions.insert({"empty", 5});
+    
+    
+    int N;
+    cin >> N;
+    
+    for (int i = 0; i < N; ++i)
+    {
+        string str; 
+        int data;
+        cin >> str;
+        
+        switch (Functions[str])
+        {
+        case 4: // all
+            Numbers = AlllNumber;
+            break;
+        case 5: // empty
+            Numbers = Empty;
+            break;
+        case 0: // add
+            cin >> data;
+            Numbers.insert(data);
+            break;
+        case 1: // remove
+            cin >> data;
+            Numbers.erase(data);
+            break;
+        case 2: // check
+            cin >> data;
+            cout << (Numbers.find(data) != Numbers.end()) << "\n";
+            break;
+        case 3: // toggle
+            cin >> data;
+            if (Numbers.find(data) != Numbers.end())
+            {
+                Numbers.erase(data);
+            }
+            else
+            {
+                Numbers.insert(data);
+            }
+            break;
+        }
+    }
+}
