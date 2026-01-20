13d8560874d25dace5dae82277388d5bc0d4e18c
PPUDANI <86819878+PPUDANI@users.noreply.github.com>
2026-01-20 21:07:17 +0900

    [Silver III] Title: 계단 오르기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.cc
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.cc
@@ -8,21 +8,22 @@ int main()
     int NumOfStair;
     cin >> NumOfStair;
     
-    int Stairs[305] = {0, };
-    int DP[305] = {0, };
+    int Stairs[301] = {0, };
+    int DP[301];
     for (int i = 1; i <= NumOfStair; i++)
     {
         cin >> Stairs[i];
     }
     
+    DP[0] = 0;
     DP[1] = Stairs[1];
     DP[2] = DP[1] + Stairs[2];
     
-    for (int i = 0; i <= NumOfStair - 2; i++)
+    for(int i = 3; i <= NumOfStair; i++)
     {
-        int firstScore = DP[i] + Stairs[i + 2];
-        DP[i + 2] = max(DP[i + 2], firstScore);
-        DP[i + 3] = max(DP[i + 3], firstScore + Stairs[i + 3]);
+        int Step1 = DP[i - 3] + Stairs[i - 1] + Stairs[i];
+        int Step2 = DP[i - 2] + Stairs[i];
+        DP[i] = max(Step1, Step2);
     }
     
     cout << DP[NumOfStair];
