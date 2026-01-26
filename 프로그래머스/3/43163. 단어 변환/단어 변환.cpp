ce91d27c4e398e9e422c5860f6856b47578988ef
PPUDANI <86819878+PPUDANI@users.noreply.github.com>
2026-01-26 20:10:13 +0900

    [level 3] Title: 단어 변환, Time: 0.04 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.cpp
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.cpp
@@ -1,54 +1,60 @@
 #include <string>
 #include <vector>
+#include <queue>
 
 using namespace std;
 
-
-bool visited[51] = {false, };
-int MinCount = 51;
-string TargetWord;
-
-void DFS(const string& word, const vector<string>& words, int count)
-{
-    if(word == TargetWord)
-    {
-        MinCount = min(count, MinCount);
-        return;
-    }
-    
-    for(int i = 0; i < words.size(); ++i)
-    {
-        if(visited[i] == true)
-        {
-            continue;
-        }
-        
-        int DifCnt = 0;
-        for(int j = 0; j < word.length(); ++j)
-        {
-            if(word[j] != words[i][j]) DifCnt++;
-        }
-
-        if(DifCnt == 1)
-        {
-            visited[i] = true;
-            DFS(words[i], words, count + 1);
-            visited[i] = false;
-        }
-    }
-    
-    return;
-}
-
+#define Max 10000
 int solution(string begin, string target, vector<string> words) 
 {
-    TargetWord = target;
-    DFS(begin, words, 0);
-    
-    if(MinCount == 51)
+    int answer = 0;
+    int wordlength = int(begin.length());
+    bool IsChangeable = false;
+    for(const string& word : words)
+    {
+        if(word == target)
+        {
+            IsChangeable = true;
+        }
+    }
+    if(IsChangeable == false)
     {
         return 0;
     }
     
-    return MinCount;
+    queue<pair<string, int>> BFS;
+    
+    BFS.push({begin, 0});
+    while(!BFS.empty())
+    {
+        string CurStr = BFS.front().first;
+        int CurCount = BFS.front().second;
+        BFS.pop();
+        
+        if(CurStr == target)
+        {
+            return CurCount;
+        }
+        
+        int WordIndex = 0;
+        for(const string& word : words)
+        {
+            int SameAlpha = 0;
+            for(int i = 0; i < wordlength; ++i)
+            {
+                if(CurStr[i] == word[i])
+                {
+                    ++SameAlpha;
+                }
+            }
+            
+            if(SameAlpha == wordlength - 1)
+            {
+                BFS.push({word, CurCount + 1});
+            }
+            ++WordIndex;
+        }
+    }
+    int b;
+    return 0;
 }
