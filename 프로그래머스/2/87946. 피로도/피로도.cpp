a4b11c802f9efe751e74c6c83030b74699887dc9
PPUDANI <86819878+PPUDANI@users.noreply.github.com>
2026-01-27 19:59:54 +0900

    [level 2] Title: 피로도, Time: 62.98 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.cpp
+++ 프로그래머스/2/87946. 피로도/피로도.cpp
@@ -1,44 +1,49 @@
 #include <iostream>
-#include <ostream>
 #include <vector>
+#include <stack>
 
 using namespace std;
 
-vector<vector<int>> Global_Dungeons;
-vector<bool> Global_Visited;
-int res = 0;
-
-int dfs(int stemina, int count)
+int solution(int k, vector<vector<int>> dungeons) 
 {
-    if(stemina)
-    for(int i = 0; i < Global_Dungeons.size(); ++i)
+    int NumOfDungeons = dungeons.size();
+    vector<bool> Visited_Base(NumOfDungeons, false);
+    stack<pair<vector<int>, vector<bool>>, vector<pair<vector<int>, vector<bool>>>> DFS;
+    
+    for(int i = 0; i < NumOfDungeons; ++i)
     {
-        if(Global_Visited[i] == false &&
-           Global_Dungeons[i][0] <= stemina && 
-           Global_Dungeons[i][1] <= stemina)
+        if(dungeons[i][0] <= k)
         {
-            Global_Visited[i] = true;
-            res = max(res, dfs(stemina - Global_Dungeons[i][1], count + 1));
-            Global_Visited[i] = false;
+            Visited_Base[i] = true;
+            vector<int> Score = {k - dungeons[i][1], 1};
+            DFS.push({Score, Visited_Base});
+            Visited_Base[i] = false;
         }
     }
     
-    return count;
+    int Count = 0;
+    while(!DFS.empty())
+    {
+        vector<int> CurScore = DFS.top().first;
+        vector<bool> CurVisited = DFS.top().second;
+        DFS.pop();
+        
+        Count = max(Count, CurScore[1]);
+        for(int i = 0; i < NumOfDungeons; ++i)
+        {
+            if(CurVisited[i] == false && dungeons[i][0] <= CurScore[0])
+            {
+                CurVisited[i] = true;
+                DFS.push({{CurScore[0] - dungeons[i][1], CurScore[1] + 1}, CurVisited} );
+                CurVisited[i] = false;
+                cout << CurScore[1] + 1 << endl;
+            }
+        }
+    }
+    
+    return Count;
 }
-
-int solution(int k, vector<vector<int>> dungeons) 
-{
-    Global_Dungeons = dungeons;
-    vector<bool> visited(dungeons.size(), false);
-    Global_Visited = visited;
-
-    dfs(k, 0);
-    return res;
-}
-
 int main()
 {
-    int a = solution(80, {{80,20}, {50, 40}, {30, 10}});
-    
-    cout << a << endl;
+    cout << solution(80,	{{80,20},{50,40},{30,10}});
 }
