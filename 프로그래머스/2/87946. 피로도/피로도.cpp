f06fa7ac41fa6ed3efde21ee9cd9b807e36b66ce
PPUDANI <86819878+PPUDANI@users.noreply.github.com>
2026-01-27 20:10:35 +0900

    [level 2] Title: 피로도, Time: 0.86 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.cpp
+++ 프로그래머스/2/87946. 피로도/피로도.cpp
@@ -1,49 +1,27 @@
-#include <iostream>
 #include <vector>
-#include <stack>
 
 using namespace std;
 
+vector<vector<int>>* GDungeons;
+bool visited[8] = {false, };
+int Count = 0;
+int dfs(int stemina, int CurCount)
+{
+    for(int i = 0; i < GDungeons->size(); ++i)
+    {
+        if(visited[i] == false && (*GDungeons)[i][0] <= stemina)
+        {
+            visited[i] = true;
+            Count = max(Count, dfs(stemina - (*GDungeons)[i][1], CurCount + 1));
+            visited[i] = false;
+        }
+    }
+    return CurCount;
+}
+
 int solution(int k, vector<vector<int>> dungeons) 
 {
-    int NumOfDungeons = dungeons.size();
-    vector<bool> Visited_Base(NumOfDungeons, false);
-    stack<pair<vector<int>, vector<bool>>, vector<pair<vector<int>, vector<bool>>>> DFS;
-    
-    for(int i = 0; i < NumOfDungeons; ++i)
-    {
-        if(dungeons[i][0] <= k)
-        {
-            Visited_Base[i] = true;
-            vector<int> Score = {k - dungeons[i][1], 1};
-            DFS.push({Score, Visited_Base});
-            Visited_Base[i] = false;
-        }
-    }
-    
-    int Count = 0;
-    while(!DFS.empty())
-    {
-        vector<int> CurScore = DFS.top().first;
-        vector<bool> CurVisited = DFS.top().second;
-        DFS.pop();
-        
-        Count = max(Count, CurScore[1]);
-        for(int i = 0; i < NumOfDungeons; ++i)
-        {
-            if(CurVisited[i] == false && dungeons[i][0] <= CurScore[0])
-            {
-                CurVisited[i] = true;
-                DFS.push({{CurScore[0] - dungeons[i][1], CurScore[1] + 1}, CurVisited} );
-                CurVisited[i] = false;
-                cout << CurScore[1] + 1 << endl;
-            }
-        }
-    }
-    
+    GDungeons = &dungeons;
+    dfs(k, 0);
     return Count;
-}
-int main()
-{
-    cout << solution(80,	{{80,20},{50,40},{30,10}});
 }
