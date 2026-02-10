cc5fe13da24b7b2a539f95f4d3f2e5941b70fc9e
PPUDANI <86819878+PPUDANI@users.noreply.github.com>
2026-02-10 21:21:59 +0900

    [level 2] Title: 기능개발, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/2/42586. 기능개발/기능개발.cpp
@@ -0,0 +1,38 @@
+#include <string>
+#include <vector>
+#include <iostream>
+using namespace std;
+
+int GetNeedDate(int _progress, int _speed)
+{
+    int Task = 100 - _progress;
+    int NeedDate = (Task / _speed) + (((Task % _speed) != 0) ? 1 : 0);
+    return NeedDate;
+}
+
+vector<int> solution(vector<int> progresses, vector<int> speeds) 
+{
+    vector<int> answer;
+    
+    int NumOfProgram = progresses.size();
+    int CurDate = GetNeedDate(progresses[0], speeds[0]);
+    int Count = 1;
+    
+    for(int i = 1; i < NumOfProgram; ++i)
+    {
+        int NeedDate = GetNeedDate(progresses[i], speeds[i]);
+        if(CurDate < NeedDate)
+        {
+            answer.push_back(Count);
+            CurDate = NeedDate;
+            Count = 1;
+        }
+        else
+        {
+            ++Count;
+        }
+    }
+    answer.push_back(Count);
+    
+    return answer;
+}
