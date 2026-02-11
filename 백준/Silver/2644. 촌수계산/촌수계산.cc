5c5bc530e7669dff2d82e4e917f3c2f8f5d6bfc1
PPUDANI <86819878+PPUDANI@users.noreply.github.com>
2026-02-11 19:48:47 +0900

    [Silver II] Title: 촌수계산, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/2644. 촌수계산/촌수계산.cc
+++ 백준/Silver/2644. 촌수계산/촌수계산.cc
@@ -5,13 +5,18 @@ using namespace std;
 
 vector<int> Graph[101];
 bool visited[101] = {false, };
+
+int n ,m;
 int Start, End;
 
-bool dfs(int CurNode, int depth, int& Count)
+int res = -1;
+
+bool dfs(int CurNode, int depth)
 {
     visited[CurNode] = true;
     if(CurNode == End)
     {
+        res = depth;
         return true;
     }
     
@@ -19,9 +24,8 @@ bool dfs(int CurNode, int depth, int& Count)
     {
         if (!visited[i])
         {
-            if (dfs(i, depth + 1, Count))
+            if (dfs(i, depth + 1))
             {
-                Count++;
                 return true;
             }
         }
@@ -35,14 +39,8 @@ int main()
     ios::sync_with_stdio(false);
     cin.tie(NULL);
     
-    int n;
-    cin >> n;
+    cin >> n >> Start >> End >> m;
     
-
-    cin >> Start >> End;
-    
-    int m;
-    cin >> m;
     for (int i = 0; i < m; ++i)
     {
         int first, second;
@@ -50,13 +48,7 @@ int main()
         Graph[first].push_back(second);
         Graph[second].push_back(first);
     }
-    int Count = 0;
-    if (dfs(Start, 0, Count))
-    {
-        cout << Count;
-    }
-    else
-    {
-        cout << -1;
-    }
+    
+    dfs(Start, 0);
+    cout << res;
 }
