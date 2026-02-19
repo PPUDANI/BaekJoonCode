510ffd56480e7e3dbf7e3df3993ba00c61f71d73
PPUDANI <86819878+PPUDANI@users.noreply.github.com>
2026-02-19 19:30:23 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 0.11 ms, Memory: 3.63 MB -BaekjoonHub

---
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.cpp
@@ -0,0 +1,45 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<vector<int>> Graph(101);
+
+int dfs(int CurNode, bool* IsVisit, int DeleteNode)
+{
+    IsVisit[CurNode] = true;
+    int size = 1;
+    for (int i : Graph[CurNode])
+    {
+        if (!IsVisit[i] && i != DeleteNode)
+        {
+            size += dfs(i, IsVisit, DeleteNode);
+        }
+    }
+    return size;
+}
+
+int CountSubTree(int StartNode, int DeleteNode)
+{
+    bool IsVisit[100] = {false, };
+    return dfs(StartNode, IsVisit, DeleteNode);
+}
+
+int solution(int n, vector<vector<int>> wires) 
+{
+    for(vector<int> wire : wires)
+    {
+        Graph[wire[0]].push_back(wire[1]);
+        Graph[wire[1]].push_back(wire[0]);
+    }
+    
+    int answer = 100;
+    for(vector<int> wire : wires)
+    {
+        int NumOfLeft = CountSubTree(wire[0], wire[1]);
+        int NumOfRight = CountSubTree(wire[1], wire[0]);
+        answer = min(answer, abs(NumOfLeft - NumOfRight));
+    }
+    
+    return answer;
+}
