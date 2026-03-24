812269bc87f916495053ace4d4a4b3ad39afd0ca
PPUDANI <86819878+PPUDANI@users.noreply.github.com>
2026-03-24 21:11:03 +0900

    [Gold V] Title: 로봇 청소기, Time: 0 ms, Memory: 2156 KB -BaekjoonHub

--- 백준/Gold/14503. 로봇 청소기/로봇 청소기.cc
+++ 백준/Gold/14503. 로봇 청소기/로봇 청소기.cc
@@ -2,18 +2,37 @@
 #include <vector>
 using namespace std;
 
+// 0: North
+// 1: East 
+// 2: South
+// 3: West
+int dy[4] = {-1, 0, 1, 0};
+int dx[4] = {0, 1, 0, -1};
+vector<vector<int>> Map;
+
+bool CrossCheck(int _Y, int _X, int CheckNum)
+{
+    for (int i = 0; i < 4; ++i)
+    {
+        if (Map[_Y + dy[i]][_X + dx[i]] == CheckNum)
+        {
+            return true;
+        }
+    }
+    return false;
+}
 int main()
 {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
     int N, M;
     cin >> N >> M;
+    Map.assign(N, vector<int>(M, 0));
     
     int StartX, StartY;
     int StartDir;
     cin >> StartY >> StartX >> StartDir;
     
-    vector<vector<int>> Map(N, vector<int>(M, 0));
     for (int i = 0; i < N; ++i)
     {
         for (int j = 0; j < M; ++j)
@@ -22,13 +41,7 @@ int main()
         }
     }
     
-    // 0: North
-    // 1: East 
-    // 2: South
-    // 3: West
-    int dy[4] = {-1, 0, 1, 0};
-    int dx[4] = {0, 1, 0, -1};
-    
+
     int CurX = StartX;
     int CurY = StartY;
     int CurDir = StartDir;
@@ -41,11 +54,7 @@ int main()
             Map[CurY][CurX] = 2;
             Count++;
         }
-        else if (
-            Map[CurY+1][CurX] != 0 &&
-            Map[CurY-1][CurX] != 0 &&
-            Map[CurY][CurX+1] != 0 &&
-            Map[CurY][CurX-1] != 0)
+        else if (!CrossCheck(CurY, CurX, 0))
         {
             // Turn Back 
             int BackDir = (CurDir + 2) % 4;
@@ -56,10 +65,7 @@ int main()
                 break;
             }
         }
-        else if (Map[CurY+1][CurX] == 0 ||
-            Map[CurY-1][CurX] == 0 ||
-            Map[CurY][CurX+1] == 0 ||
-            Map[CurY][CurX-1] == 0)
+        else if (CrossCheck(CurY, CurX, 0))
         {
             // Turn Left 
             CurDir = (CurDir + 3) % 4;
