faa9e2c47fa150b4a15c0474ac282e63043afe2a
PPUDANI <86819878+PPUDANI@users.noreply.github.com>
2026-03-24 22:00:14 +0900

    [Silver I] Title: 미로 탐색, Time: 0 ms, Memory: 2160 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.cc
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.cc
@@ -1,82 +1,66 @@
 #include <iostream>
 #include <vector>
 #include <queue>
-#include <stack>
-
+#include <string>
 using namespace std;
 
-class Node
+#define IS_VISIT 2;
+
+struct Node
 {
-public:
-    int x;
-    int y;
-    
-    Node operator +(const Node& _Node)
-    {
-        Node Res;
-        Res.x = this->x + _Node.x;
-        Res.y = this->y + _Node.y;
-        return Res;
-    }
+    int Y;
+    int X;
+    int Dist;
 };
 
 int main()
 {
     ios::sync_with_stdio(false);
-    cin.tie(0);
-    
+    cin.tie(NULL);
+
     int N, M;
+    
     cin >> N >> M;
-    vector<vector<int>> Map(N+1, vector<int>(M+1, 0));
+    
+    vector<vector<int>> Map(N + 2, vector<int>(M + 2, 0));
     for (int i = 1; i <= N; ++i)
     {
-        string str;
-        cin >> str;
+        string s;
+        cin >> s;
         for (int j = 1; j <= M; ++j)
         {
-            Map[i][j] = str[j - 1] - '0';
+            Map[i][j] = s[j - 1] - '0';
         }
     }
     
-    vector<Node> Dir = 
-    {
-    {-1, 0}, // Left
-    {1, 0}, // Right
-    {0, 1}, // Up
-    {0, -1} // Down
-    };
+    int dx[4] = {-1, 1, 0, 0};
+    int dy[4] = {0, 0, -1, 1};
     
-    // 현재 위치를 저장해야하고
-    // 방문 저장.
-    queue<pair<Node, int>> Q;
-    vector<vector<bool>> Visit(N + 1, vector<bool>(M + 1, false));
-    Q.push({{1, 1}, 1});
-    Visit[1][1] = true;
+    queue<Node> q;
+    vector<vector<bool>> IsVisited(N + 2, vector<bool>(M + 2, false));
+    IsVisited[1][1] = true;
+    q.push({1, 1, 1});
     
-    while (!Q.empty())
+    while (!q.empty())
     {
-        Node CurNode = Q.front().first;
-        int CurCount = Q.front().second;
-        Q.pop();
+        Node CurNode = q.front();
+        q.pop();
         
-        if (CurNode.x == M && CurNode.y == N)
+        if (CurNode.Y == N && CurNode.X == M)
         {
-            cout << CurCount << endl;
+            cout << CurNode.Dist;
             return 0;
         }
         
-        for (int i = 0; i < Dir.size(); ++i)
+        for (int i = 0; i < 4; ++i)
         {
-            Node Next = Dir[i] + CurNode;
-            if (Next.y > N || Next.y <= 0 || Next.x > M || Next.x <= 0 || Map[Next.y][Next.x] == 0)
+            int NextY = CurNode.Y + dy[i];
+            int NextX = CurNode.X + dx[i];
+            if (Map[NextY][NextX] == 1 &&
+                IsVisited[NextY][NextX] == false)
             {
-                continue;
-            }
-            
-            if (Visit[Next.y][Next.x] == false)
-            {
-                Visit[Next.y][Next.x] = true;
-                Q.push({Next, CurCount + 1});
+                IsVisited[NextY][NextX] = true;
+                q.push({NextY, NextX, CurNode.Dist + 1});
             }
         }
     }
